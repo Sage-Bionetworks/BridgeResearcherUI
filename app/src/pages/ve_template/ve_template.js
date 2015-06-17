ad260137dec5fb7f7fb3fb42dba59cf490931a1a
alxdarksage <alx.dark@sagebase.org>
2015-06-17 12:48:38 -0700

    Fixing typo

--- app/src/pages/ve_template/ve_template.js
+++ app/src/pages/ve_template/ve_template.js
@@ -1,6 +1,6 @@
 var ko = require('knockout');
 var serverService = require('../../services/server_service');
-var util = require('../../utils');
+var utils = require('../../utils');
 
 module.exports = function() {
     var self = this;
