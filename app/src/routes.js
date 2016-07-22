5f7555cf4e0c3c95d12b998bae33610f37780a4a
alxdarksage <alx.dark@sagebase.org>
2016-07-22 11:24:59 -0700

    Some further work on the uploads page.

--- app/src/routes.js
+++ app/src/routes.js
@@ -19,7 +19,6 @@ var SCHEMAID_REVISION = ['schemaId','revision'];
 var SCHEMAID = ['schemaId'];
 var USERID_NAME = ['userId', 'name'];
 var USERID_IDENTIFIER_NAME = ['userId','identifier','name'];
-var USERID_UPLOADID_NAME = ['userId','uploadId','name'];
 var USERID = ['userId'];
 
 function namedParams(fields, args) {
@@ -74,7 +73,6 @@ router.on('/participants/:userId/reports/:identifier/:name', routeTo('participan
 router.on('/participants/:userId/activities/:name', routeTo('participant_activities', USERID_NAME));
 router.on('/participants/:userId/consents/:name', routeTo('participant_consents', USERID_NAME));
 router.on('/participants/:userId/reports/:name', routeTo('participant_reports', USERID_NAME));
-router.on('/participants/:userId/uploads/:uploadId/:name', routeTo('participant_upload', USERID_UPLOADID_NAME));
 router.on('/participants/:userId/uploads/:name', routeTo('participant_uploads', USERID_NAME));
 router.on('/participants/:userId/:name', routeTo('participant', USERID_NAME));
 router.on('/participants/:userId', routeTo('participant', USERID));
