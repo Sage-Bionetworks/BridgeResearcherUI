bfd84b52791ca860480d7f839199d39deaa880a5
alxdarksage <alx.dark@sagebase.org>
2016-10-21 16:15:19 -0700

    Fix-ups to the uploads progress indicator for some states not covered.

--- app/src/utils.js
+++ app/src/utils.js
@@ -85,18 +85,24 @@ function createEmailTemplate(email, identifier) {
     }
     return parts[0] + "+" + identifier + "@" + parts[1];
 }
-function getDateRange(dayOffset) {
-    dayOffset = dayOffset || 0;
+function getDateRange(date) {
+    date = (date) ? new Date(date) : new Date();
+    var obj = {
+        startTime: formatLocalDate(date, "00:00:00.000"), 
+        endTime: formatLocalDate(date, "23:59:59.999")
+    };
+    console.log(obj);
+    return obj;
+}
+function pad(num) {
+    var norm = Math.abs(Math.floor(num));
+    return (norm < 10 ? '0' : '') + norm;
+}
+function formatLocalDate(now, timePortion) {
+    var tzo = -now.getTimezoneOffset(), dif = tzo >= 0 ? '+' : '-';
 
-    var end = new Date();
-    end.setDate(end.getDate()+dayOffset);
-    end.setHours(23,59,59,999);
-
-    var start = new Date();
-    start.setDate(start.getDate()+dayOffset);
-    start.setHours(0,0,0,0);
-
-    return {startTime: start.toISOString(), endTime: end.toISOString()};
+    return now.getFullYear() + '-' + pad(now.getMonth()+1) + '-' + pad(now.getDate()) +
+        'T' + timePortion + dif + pad(tzo / 60) + ':' + pad(tzo % 60);
 }
 
 /**
