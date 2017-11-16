6acbdab4c0b7328b63b70bd3705cd2d7b65480da
Alx Dark <alx.dark@sagebase.org>
2017-11-15 16:26:57 -0800

    Cleanup.

--- app/src/pages/settings/applinks.js
+++ app/src/pages/settings/applinks.js
@@ -53,7 +53,6 @@ module.exports = function() {
     self.save = function(vm, event) {
         self.study = binder.persist(self.study);
 
-        console.log("study", self.study);
         utils.startHandler(vm, event);
         serverService.saveStudy(self.study)
             .then(utils.successHandler(vm, event, "Study information saved."))
@@ -87,20 +86,6 @@ module.exports = function() {
     };
 
     serverService.getStudy()
-        .then(function(study) {
-            /*
-            study.appleAppLinks.push({
-                appID: 'fooboo.car', 
-                paths: ['/fooboo/','/fooboo/*']
-            });
-            study.androidAppLinks.push({
-                namespace: 'namespace',
-                package_name: 'org.sagebionetworks.bridge',
-                sha256_cert_fingerprints: ['14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5','14:6D:E9:83:C5:73:06:50:D8:EE:B9:95:2F:34:FC:64:16:A0:83:42:E6:1D:BE:A8:8A:04:96:B2:3F:CF:44:E5']
-            });
-            */
-            return study;
-        })
         .then(binder.assign('study'))
         .then(binder.update())
         .catch(utils.failureHandler());
