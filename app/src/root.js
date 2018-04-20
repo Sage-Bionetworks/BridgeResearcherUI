3026dbce57fb10cbc0dd062fc128cd3775c0ca01
Alx Dark <alx.dark@sagebase.org>
2018-04-20 12:10:19 -0700

    Change JSON formatter so it will work on strings, including upload data field

--- app/src/root.js
+++ app/src/root.js
@@ -120,7 +120,6 @@ let RootViewModel = function() {
         self.rolesObs([]);
         self.openDialog('sign_in_dialog', {closeable:false});
     });
-    // TODO: This is running every second all the time?!
     setTimeout(checkVerifyStatus, 1000);
 };
 
