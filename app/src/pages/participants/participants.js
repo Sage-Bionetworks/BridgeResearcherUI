d9ae2654ca199afc585c5d06d7a1c5d5a6168497
Alx Dark <alx.dark@sagebase.org>
2018-06-07 13:08:25 -0700

    Change the action menu on users based on changes we've made to allow reset password on unverified accounts.

--- app/src/pages/participants/participants.js
+++ app/src/pages/participants/participants.js
@@ -199,7 +199,7 @@ module.exports = function() {
         return item.status === 'unverified';
     };
     self.resetPwdVisible = function(item) {
-        return true;
+        return item.status !== 'disabled';
     };
     self.enableVisible = function(item) {
         return item.status === 'disabled' && root.isAdmin();
@@ -208,7 +208,7 @@ module.exports = function() {
         return item.status === 'enabled' && root.isAdmin();
     };
     self.signOutVisible = function(item) {
-        return item.status !== 'unverified';
+        return item.status !== 'disabled' && item.status !== 'unverified';
     };
     self.deleteVisible = function(item) {
         return root.isDeveloper() && root.isResearcher();
