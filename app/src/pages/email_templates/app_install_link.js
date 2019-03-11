b9a94b20205f5283956da8d5b0fcf15ff99730b4
Alx Dark <alx.dark@sagebase.org>
2019-03-11 14:48:45 -0700

    And remove more code

--- app/src/pages/email_templates/app_install_link.js
+++ app/src/pages/email_templates/app_install_link.js
@@ -4,10 +4,4 @@ module.exports = class AppInstallLinkTemplate extends EmailViewModel {
   constructor() {
     super("appInstallLinkTemplate");
   }
-  postLoad(study) {
-    this.autoVerificationEnabledObs(!study.autoVerificationEmailSuppressed);
-  }
-  preSave(study) {
-    study.autoVerificationEmailSuppressed = !this.autoVerificationEnabledObs();
-  }
 };
