94cd50de49e50ac741cc51fb9437f0f2a243d388
Alx Dark <alx.dark@sagebase.org>
2019-03-11 14:47:49 -0700

    Remove viewModel code for flag that doesn't apply to app install links.

--- app/src/pages/email_templates/app_install_link.js
+++ app/src/pages/email_templates/app_install_link.js
@@ -1,15 +1,8 @@
 import EmailViewModel from "./email_view_model";
-import ko from "knockout";
 
 module.exports = class AppInstallLinkTemplate extends EmailViewModel {
   constructor() {
     super("appInstallLinkTemplate");
-    this.autoVerificationEnabledObs = ko.observable();
-    this.autoVerificationEnabledObs.subscribe(newValue => {
-      if (this.editor) {
-        this.editor.setReadOnly(!newValue);
-      }
-    });
   }
   postLoad(study) {
     this.autoVerificationEnabledObs(!study.autoVerificationEmailSuppressed);
