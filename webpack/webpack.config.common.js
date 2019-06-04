dcce3d4b71a77d503c2aaa03d7910f627c01bd25
Alx Dark <alx.dark@sagebase.org>
2019-06-04 10:21:09 -0700

    Trial #2934

--- webpack/webpack.config.common.js
+++ webpack/webpack.config.common.js
@@ -21,7 +21,13 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: {
+          loader: 'babel-loader', 
+          options: {
+            "presets": [["@babel/env", { "modules": false }]],
+            "plugins": ["add-module-exports"]
+          }
+        }
       },
       {
         test: /\.(scss|css)$/,
@@ -29,7 +35,13 @@ module.exports = {
       },
       {
         test: /\.html$/, 
-        use: {loader: 'html-loader', options: {removeComments: false, minimize: true}}
+        use: {
+          loader: 'html-loader', 
+          options: {
+            removeComments: false, 
+            minimize: true
+          }
+        }
       }    
     ]
   }
