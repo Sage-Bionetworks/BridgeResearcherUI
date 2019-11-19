da913211a7b1bbd17c6bbb884b6fce1c77d44075
Alx Dark <alx.dark@sagebase.org>
2019-11-19 11:25:22 -0800

    cleanpu

--- app/src/pages/survey/constraints/height_constraints.js
+++ app/src/pages/survey/constraints/height_constraints.js
@@ -4,6 +4,5 @@ import surveyUtils from "../survey_utils";
 export default function(params) {
   let self = this;
   surveyUtils.initConstraintsVM(self, params);
-  self.forInfantObs = self.element.constraints.forInfantObs;
   fn.copyProps(self, self.element.constraints, "forInfantObs", "requiredObs");
 };
