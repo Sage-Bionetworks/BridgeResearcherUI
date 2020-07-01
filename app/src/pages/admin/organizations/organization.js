2286f032b644cd44716ef2b0e1f26f9754b799a4
Alx Dark <alx.dark@sagebase.org>
2020-07-01 14:23:51 -0700

    Cleanup

--- app/src/pages/admin/organizations/organization.js
+++ app/src/pages/admin/organizations/organization.js
@@ -104,10 +104,6 @@ export default function organization(params) {
     refresh: () => loadMembers(self.query)
   });
 
-  function formatPhone(phone) {
-    return fn.flagForRegionCode(phone.regionCode) + " " + phone.nationalFormat;
-  }
-
   let mapItems = (response) => {
     response.items.forEach((acct) => fn.formatNameAsFullLabel(acct));
     return response;
