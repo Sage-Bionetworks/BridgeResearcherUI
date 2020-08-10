d89bb9987aa4af9be384f2951a094cec803fbce6
Alx Dark <alx.dark@sagebase.org>
2020-08-10 15:57:01 -0700

    Addressing security alerts.

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -1,10 +1,14 @@
 'use strict';
 
 const common = require('./webpack.config.common');
+const path = require('path');
 
 console.log('[Webpack] Use dev configuration\n');
 
 module.exports = Object.assign({}, {
     mode: 'development',
     devtool: '#source-map',
+    devServer: {
+        contentBase: 'app'
+    }
 }, common);
