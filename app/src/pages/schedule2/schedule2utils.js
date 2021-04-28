8739c0a753c0dec668f99b2f8d47d9872b03bbe2
Alx Dark <alx.dark@sagebase.org>
2021-04-27 17:01:04 -0700

    fix

--- app/src/pages/schedule2/schedule2utils.js
+++ app/src/pages/schedule2/schedule2utils.js
@@ -7,7 +7,9 @@ const SORTER = fn.makeFieldSorter("text");
 export function getEventIds() {
   return serverService.getApp().then(app => {
     var array = SYSTEM_EVENTS.map(s => ({text: s, value: s}));
-    array = array.concat(app.activityEventKeys.map(s => ({text: s, value: 'custom:'+s})));
+    Object.keys(app.customEvents).forEach(key => {
+      array.push({text: key, value: 'custom:'+key})
+    });
     array.sort(SORTER);
     return array;
   });
