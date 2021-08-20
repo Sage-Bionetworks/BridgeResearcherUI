20212049d4ec9dafd955063fc47163871b93f6a1
Alx Dark <alx.dark@sagebase.org>
2021-08-20 10:42:18 -0700

    fixing sponsor dialog to be paged

--- app/src/dialogs/add_sponsored_study/add_sponsored_study.js
+++ app/src/dialogs/add_sponsored_study/add_sponsored_study.js
@@ -5,36 +5,50 @@ import serverService from '../../services/server_service';
 import tables from '../../tables';
 import utils from '../../utils';
 
+const PAGE_KEY = 'asp-refresh';
+
 export default function(params) {
   const self = this;
 
   self.closeDialog = params.closeFunc;
+  self.sponsoredStudiesIds = new Set();
+
+  self.query = {};
 
   tables.prepareTable(self, {
     name: "study",
     id: "add_sponsored_study",
-    refresh: () => ko.postbox.publish("page-refresh")
+    refresh: () => ko.postbox.publish(PAGE_KEY)
   });
 
   self.addAndClose = (vm, event) => {
     utils.startHandler(vm, event);
 
     let fn = (study) => serverService.addSponsor(study.identifier, params.orgId);
-    let adds = self.itemsObs().filter(acct => acct.checkedObs());
+    let adds = self.itemsObs().filter(item => 
+      item.checkedObs() && !self.sponsoredStudiesIds.has(item.identifier));
     Promise.each(adds, (org) => fn(org))
       .then(params.closeFunc)
       .catch(utils.failureHandler({ id: "add_sponsored_study" }));
   };
+  self.isPermanentlyChecked = function(item) {
+    return self.sponsoredStudiesIds.has(item.identifier);
+  }
 
-  self.sponsoredStudiesIds = new Set();
-
-  serverService.getSponsoredStudies(params.orgId, {offsetBy: 0, pageSize: 100})
-    .then((response) => self.sponsoredStudiesIds = new Set(response.items.map(study => study.identifier)))
-    .then(() => serverService.getStudies(0, 100))
-    .then((response) => {
-      response.items = response.items.filter(item => !self.sponsoredStudiesIds.has(item.identifier));
-      return response;
-    })
-    .then(fn.handleObsUpdate(self.itemsObs, "items"))
-    .catch(utils.failureHandler({ id: "add_sponsored_study" }));
+  self.load = function(offsetBy) {
+    self.query.offsetBy = offsetBy;
+    return serverService.getSponsoredStudies(params.orgId, {offsetBy: 0, pageSize: 100})
+      .then((response) => self.sponsoredStudiesIds = new Set(response.items.map(study => study.identifier)))
+      .then(() => serverService.getStudies(self.query))
+      .then(fn.handleObsUpdate(self.itemsObs, "items"))
+      .then((response) => {
+        response.items.forEach(item => {
+          item.checkedObs(self.sponsoredStudiesIds.has(item.identifier));
+        });
+        return response;
+      })
+      .then(fn.handleObsUpdate(self.itemsObs, "items"))
+      .catch(utils.failureHandler({ id: "add_sponsored_study" }));
+  }
+  setTimeout(() => ko.postbox.publish(PAGE_KEY), 1);
 }
