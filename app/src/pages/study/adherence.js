0ecc7c602104036eb52651e971f0cde68d9a60eb
Alx Dark <alx.dark@sagebase.org>
2022-02-04 10:06:17 -0800

    Cleanup

--- app/src/pages/study/adherence.js
+++ app/src/pages/study/adherence.js
@@ -1,9 +1,16 @@
 import BaseStudy from "./base_study";
 import fn from "../../functions";
 import ko from "knockout";
-import root from "../../root";
 import serverService from "../../services/server_service";
+import storeService from "../../services/store_service";
 import tables from "../../tables";
+import utils from "../../utils";
+
+const PAGE_KEY = 'adh-refresh';
+
+function decode(str = '') {
+  return decodeURIComponent(str).replace('+', ' ');
+}
 
 export default class StudyAdherence extends BaseStudy {
   constructor(params) {
@@ -11,64 +18,49 @@ export default class StudyAdherence extends BaseStudy {
     this.studyId = params.studyId;
     this.study = null;
   
-    fn.copyProps(this, fn, 'formatDate', 'formatDateTime', 'formatNameAsFullLabel');
-    fn.copyProps(this, root, 'isAdmin');
-    
+    fn.copyProps(this, fn, 'formatNameAsFullLabel');
+
     this.postLoadPagerFunc = fn.identity;
     this.postLoadFunc = (func) => this.postLoadPagerFunc = func;
+    this.search = storeService.restoreQuery("adh", "labelFilters", "progressionFilters");
 
     this.binder
       .obs('label')
-      .obs('test', 'both')
-      .obs('formattedSearch', '')
+      .obs('formattedSearch')
+      .obs('test')
       .obs('id')
-      .obs('range', '0-100')
+      .obs('range')
       .obs('unstarted')
       .obs('inProgress')
       .obs('done');
+    this.updateObservers();
 
     tables.prepareTable(this, {
       name: "weekly adherence report",
       type: "Weekly Adherence Report",
       id: "adherence",
-      refresh: () => ko.postbox.publish('adh-refresh', 0)
+      refresh: () => ko.postbox.publish(PAGE_KEY, 0)
     });
     this.loadingFunc = this.loadingFunc.bind(this);
-    super.load().then(() => ko.postbox.publish('adh-refresh', 0));
+    super.load().then(() => ko.postbox.publish(PAGE_KEY, 0));
   }
-  loadingFunc(offsetBy) {
-    let query = {offsetBy, pageSize: 50};
-    query.testFilter = this.testObs();
-    let range = this.rangeObs();
-    if (/^\d{1,3}-\d{1,3}$/.test(range)) {
-      let parts = range.split('-');
-      query.adherenceMin = parseInt(parts[0]);
-      query.adherenceMax = parseInt(parts[1]);
+  updateObservers() {
+    let range = null;
+    if (this.search.adherenceMin || this.search.adherenceMax) {
+      range = this.search.adherenceMin || 0;
+      range += '-';
+      range += this.search.adherenceMax || 100;
     }
-    if (this.labelObs()) {
-      query.labelFilters = [this.labelObs()];
-    }
-    if (this.idObs()){ 
-      query.idFilter = this.idObs();
-    }
-    let pf = [];
-    if (this.unstartedObs()) {
-      pf.push('unstarted');
-    }
-    if (this.inProgressObs()) {
-      pf.push('in_progress');
-    }
-    if (this.doneObs()) {
-      pf.push('done');
-    }
-    if (pf.length) {
-      query.progressionFilters = pf;
-    }
-    return serverService.getStudyParticipantAdherenceReports(this.studyId, query)
-      .then(this.postLoadPagerFunc)
-      .then(this.formatSearch.bind(this))
-      .then(fn.handleObsUpdate(this.itemsObs, "items"))
-      .catch(this.failureHandler);
+    let label = this.search.labelFilters ? decode(this.search.labelFilters[0]) : null;
+    let pf = this.search.progressionFilters || [];
+    this.binder
+      .obs('label', label)
+      .obs('test', this.search.testFilter)
+      .obs('id', decode(this.search.idFilter))
+      .obs('range', range)
+      .obs('unstarted', pf.includes('unstarted'))
+      .obs('inProgress', pf.includes('in_progress'))
+      .obs('done', pf.includes('done'));
   }
   formatActivity(entry) {
     if (entry.studyBurstId) {
@@ -82,7 +74,7 @@ export default class StudyAdherence extends BaseStudy {
     }
     return 'Next activity on ' + this.formatDate(entry.startDate);
   }
-  formatSearch(res) {
+  formatSearch() {
     let array = [];
     if (this.idObs()) {
       array.push(`identifier includes “${this.idObs()}”`);
@@ -93,46 +85,98 @@ export default class StudyAdherence extends BaseStudy {
     if (this.testObs() && this.testObs() !== 'both') {
       array.push(`accounts are ${this.testObs()} accounts`);
     }
-    if (res.requestParams.progressionFilters) {
-      let s = res.requestParams.progressionFilters
-        .map(s => s.replace('_', ' '))
-        .join(', ');
-      array.push(`study is ${s} for user`);
+    let states = [];
+    if (this.unstartedObs()) states.push('unstarted');
+    if (this.inProgressObs()) states.push('in progress');
+    if (this.doneObs()) states.push('done');
+    if (states.length) {
+      array.push(`study is ${states.join(', ')} for user`);
     }
     let range = this.rangeObs();
     if (range !== '0-100' && /^\d{1,3}-\d{1,3}$/.test(range)) {
       array.push(`adherence is in range ${range}%`);
     }
     if (array.length) {
-      this.formattedSearchObs('Search for all reports where ' + 
-        fn.formatList(array, "and", ";"));
-    } else {
-      this.formattedSearchObs('Search for all reports');  
+      return 'Search for all reports where ' + fn.formatList(array, "and", ";")
     }
-    return res;
+    return 'Search for all reports';
   }
   formatProgression(entry) {
-    return (entry.progression === 'unstarted') ? '— Unstarted —' : '— Done —';
+    if (entry.progression === 'unstarted') {
+      return '— Unstarted —';
+    } else if (entry.progression === 'done') {
+      return '— Done —';
+    }
+    return '— No Schedule —';
   }
   formatWin(report, day, index) {
     let entry = report.byDayEntries[day][index];
-    console.log(entry);
     return entry.timeWindows
       .map(win => `<span title="${entry.startDate}" class="bar ${win.state}"></span>`)
       .join('');
   }
-  doFormSearch() {
-    ko.postbox.publish('adh-refresh', 0);
+  updateSearch() {
+    let search = {};
+    if (this.testObs() !== '') {
+      search.testFilter = this.testObs();
+    }
+    let range = this.rangeObs();
+    if (/^\d{1,3}-\d{1,3}$/.test(range)) {
+      let parts = range.split('-');
+      search.adherenceMin = parseInt(parts[0]);
+      search.adherenceMax = parseInt(parts[1]);
+    } else if (/^\d{1,3}$/.test(range)) {
+      search.adherenceMin = 0;
+      search.adherenceMax = parseInt(range);
+    } else {
+      this.rangeObs(null);
+    }
+    if (this.labelObs()) {
+      search.labelFilters = [this.labelObs()];
+    }
+    if (this.idObs()){ 
+      search.idFilter = this.idObs();
+    }
+    let pf = [];
+    if (this.unstartedObs()) pf.push('unstarted');
+    if (this.inProgressObs()) pf.push('in_progress');
+    if (this.doneObs()) pf.push('done');
+    if (pf.length) {
+      search.progressionFilters = pf;
+    }
+    this.search = search;
+    return search;
+  }
+  loadingFunc(offsetBy) {
+    this.search.offsetBy = offsetBy;
+    this.search = this.updateSearch();
+
+    storeService.persistQuery("adh", this.search);
+    this.formattedSearchObs(this.formatSearch());
+
+    utils.clearErrors();
+    return serverService.getStudyParticipantAdherenceReports(this.studyId, this.search)
+      .then(this.postLoadPagerFunc)
+      .then(fn.handleObsUpdate(this.itemsObs, "items"))
+      .catch(this.failureHandler);
+  }
+  doFormSearch(vm, event) {
+    if (event.keyCode === 13)  {
+      ko.postbox.publish(PAGE_KEY, 0);
+    }
+  }
+  searchButton() {
+    ko.postbox.publish(PAGE_KEY, this.search.offsetBy);
   }
   clear() {
-    this.testObs('both');
-    this.labelObs('');
-    this.rangeObs('0-100');
-    this.formattedSearchObs('');
-    this.idObs('');
+    this.testObs(null);
+    this.labelObs(null);
+    this.rangeObs(null);
+    this.formattedSearchObs(null);
+    this.idObs(null);
     this.unstartedObs(false);
     this.inProgressObs(false);
     this.doneObs(false);
-    ko.postbox.publish('adh-refresh', 0);
+    ko.postbox.publish(PAGE_KEY, 0);
   }
 }
